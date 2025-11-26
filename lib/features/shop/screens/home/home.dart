65cc07f29aac02b5c4c7a2d0e59b546e9db93d61
abKder <abdulkader.im.run@gmail.com>
2025-11-26 22:56:42 +0600

    add home categories

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,9 +1,7 @@
-import 'package:e_commerce/common/widgets/button/custom_shapes/circular_container.dart';
 import 'package:e_commerce/features/shop/screens/home/widgets/home_appbar.dart';
+import 'package:e_commerce/features/shop/screens/home/widgets/home_categories.dart';
 import 'package:e_commerce/features/shop/screens/home/widgets/primary_header_container.dart';
-import 'package:e_commerce/utils/constants/colors.dart';
 import 'package:e_commerce/utils/constants/sizes.dart';
-import 'package:e_commerce/utils/constants/texts.dart';
 import 'package:flutter/material.dart';
 import '../../../../common/widgets/textfields/search_bar.dart';
 import '../../../../utils/helpers/helperFunction.dart';
@@ -30,37 +28,7 @@ class HomeScreen extends StatelessWidget {
             SizedBox(height: ASizes.spaceBtwSections),
 
             // home categories
-            Text(ATexts.popularCategories,
-                style: Theme.of(context)
-                    .textTheme
-                    .headlineSmall!
-                    .apply(color: AColors.white)),
-
-            SizedBox(height: ASizes.spaceBtwItems / 2),
-
-            SizedBox(
-              height: 10,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: 10,
-                itemBuilder: (context, index) {
-                  return Column(
-                    children: [
-                      ACircularContainer(height: 80, width: 75),
-                      SizedBox(
-                          width: 55,
-                          child: Text("Sport Categories",
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .labelMedium!
-                                  .apply(color: AColors.white),
-                              overflow: TextOverflow.ellipsis,
-                              textAlign: TextAlign.center)),
-                    ],
-                  );
-                },
-              ),
-            )
+            AHomeCategories()
           ],
         )),
 
---
+++ lib/features/shop/screens/home/widgets/home_categories.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import '../../../../../common/widgets/button/custom_shapes/circular_container.dart';
+import '../../../../../utils/constants/colors.dart';
+import '../../../../../utils/constants/sizes.dart';
+import '../../../../../utils/constants/texts.dart';
+
+class AHomeCategories extends StatelessWidget {
+  const AHomeCategories({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(ATexts.popularCategories,
+            style: Theme.of(context)
+                .textTheme
+                .headlineSmall!
+                .apply(color: AColors.white)),
+        SizedBox(height: ASizes.spaceBtwItems / 2),
+        SizedBox(
+          height: 10,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: 10,
+            itemBuilder: (context, index) {
+              return Column(
+                children: [
+
+                  //circular image
+                  ACircularContainer(height: 80, width: 75),
+
+                  //title
+                  SizedBox(
+                      width: 55,
+                      child: Text("Sport Categories",
+                          style: Theme.of(context)
+                              .textTheme
+                              .labelMedium!
+                              .apply(color: AColors.white),
+                          overflow: TextOverflow.ellipsis,
+                          textAlign: TextAlign.center)),
+                ],
+              );
+            },
+          ),
+        )
+      ],
+    );
+  }
+}
